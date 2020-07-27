11ac9b9d25cb78ac217c07671a039af1db9cbe1c
Raphael Brito <brito.raphaelc@gmail.com>
2020-07-26 23:37:47 -0300

    Login page with your contents

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -27,8 +28,8 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
+        <Route path='/Login' component={Login} exact={true} />
+        <Route exact path='/' render={() => <Redirect to='/Login' />} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,2 +1,47 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonInput,
+  IonButton,
+  IonImg,
+  IonThumbnail,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonIcon,
+} from '@ionic/react';
 import React from 'react';
 import './Login.css';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent class='.ion-justify-content-start ion-padding-start'>
+        <IonImg class='container' src={require('./../assets/icon.png')} />
+        <div>
+          <h3>Proceed with your</h3>
+          <h1>Login</h1>
+        </div>
+
+        <IonGrid>
+          <IonRow class='ion-justify-content-center'>
+            <IonCol size='auto'>
+              <IonInput class='input' placeholder='GitHub Username'></IonInput>
+              <IonIcon name='person' slot='end'></IonIcon>
+            </IonCol>
+          </IonRow>
+          <IonRow class='ion-justify-content-center'>
+            <IonCol size='auto'>
+              <IonButton>Login</IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
