c609c16457e55ee3df2100b3d835d533cb0758a1
Raphael Brito <brito.raphaelc@gmail.com>
2020-07-27 22:08:06 -0300

    updating routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import PullRequests from './pages/PullRequests';
 
 const Routes = () => {
   return (
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route path='/Login' component={Login} exact={true} />
+        <Route exact path='/Login' component={Login} />
         <Route exact path='/' render={() => <Redirect to='/Login' />} />
+        <Route exact path='/PullRequests' component={PullRequests} />
+        <Route exact path='/Home/:id' component={Home} />
       </IonRouterOutlet>
     </IonReactRouter>
   );
